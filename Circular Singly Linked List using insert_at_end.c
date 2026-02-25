301f35e6097e6aa3d5f3188c6c9daa2101f4bca3
ADARSH S NAIR <141129505+adarshnair-2@users.noreply.github.com>
2026-02-25 14:09:32 +0530

    Implement circular singly linked list with insertion

---
+++ Circular Singly Linked List using insert_at_end.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void insert_at_end(int data)
+{
+    struct node *newnode, *temp;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = data;
+
+    if (head == NULL)
+    {
+        newnode->next = newnode;
+        head = newnode;
+    }
+    else
+    {
+        temp = head;
+
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newnode;
+        newnode->next = head;
+    }
+}
+
+void traverse()
+{
+    struct node *temp;
+   
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+    }
+    else
+    {
+        temp = head;
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        while (temp != head);
+       
+        printf("\n");
+    }
+}
+
+int main()
+{
+    insert_at_end(10);
+    insert_at_end(20);
+    insert_at_end(30);
+   
+    traverse();
+   
+    return 0;
+}
